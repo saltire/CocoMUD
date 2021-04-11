a73136db8a8aa84ee7415723b9cb7726386eef35
saltire sable <saltiresable@gmail.com>
2021-04-11 13:53:56 -0400

    Working discord bot with ping/pong example

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Discord = require('discord.js');
+
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  console.log('Got message:', msg);
+
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
