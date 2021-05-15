512c1336dbc33d46e5f53fc5b3d0135a9dc495c2
saltire sable <saltiresable@gmail.com>
2021-05-15 09:39:21 -0400

    Font rendering, discord embed

---
+++ font.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const sharp = require('sharp');
+
+
+const edscii = [
+  ` !"#$%&'()*+,-./`,
+  `0123456789:'<=>?`,
+  `@abcdefghijklmno`,
+  `pqrstuvwxyz[£]✓π`,
+  `█ABCDEFGHIJKLMNO`,
+  `PQRSTUVWXYZ♠♥♣♦●`,
+];
+
+module.exports = {
+  async getFont() {
+    const font = sharp('./c64_edscii.png');
+    const chars = {};
+
+    await Promise.all(edscii.map(async (row, y) => {
+      await Promise.all(row.split('').map(async (char, x) => {
+        chars[char] = await font
+          .extract({
+            left: x * 8,
+            top: y * 8,
+            width: 8,
+            height: 8,
+          })
+          .toBuffer();
+      }));
+    }));
+
+    return chars;
+  },
+
+  async renderLine(chars, message) {
+    const lines = message.split('\n');
+    return sharp(
+      {
+        create: {
+          width: Math.max(...lines.map(line => line.length)) * 8,
+          height: lines.length * 8,
+          channels: 3,
+          background: { r: 0, g: 0, b: 0 },
+        },
+      })
+      .composite(lines.flatMap((line, y) => line.split('').map((char, x) => ({
+        input: chars[char] || chars[' '],
+        top: y * 8,
+        left: x * 8,
+      }))))
+      .png()
+      .toBuffer();
+  },
+};
--- index.js
+++ index.js
@@ -2,18 +2,43 @@
 
 const Discord = require('discord.js');
 
+const font = require('./font');
+
 
 const client = new Discord.Client();
 
-client.on('ready', () => {
+let chars;
+
+client.on('ready', async () => {
+  chars = await font.getFont();
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on('message', async msg => {
   console.log('Got message:', msg);
+  if (msg.channel.type === 'dm' && msg.author.tag !== client.user.tag) {
 
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+    const imgBuffer = await font.renderLine(chars, msg.content);
+    // msg.reply(new Discord.MessageAttachment(imgBuffer));
+
+    // if (msg.member && msg.content.startsWith('!nick'))
+
+    msg.reply({
+      content: 'Content',
+      files: [new Discord.MessageAttachment(imgBuffer, 'font.png')],
+      embed: {
+        description: 'Here is your text in C64 format.',
+        image: {
+          url: 'attachment://font.png',
+        },
+      },
+    })
+  }
+
+  if (msg.author.tag === client.user.tag) {
+    msg.react('😊');
+    msg.react('😍');
+    msg.react('✨');
   }
 });
 
